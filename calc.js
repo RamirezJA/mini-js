a0bb2c9059e6cd1be086e1a492a339b46209ce04
RamirezJA <26813102+RamirezJA@users.noreply.github.com>
2021-01-12 22:17:45 -0800

    16:52

---
+++ calc.js
@@ -0,0 +1,19 @@
+let prevVal = ''
+let newVal = ''
+let resultVal = ''
+let mathOperator = ''
+let decimalClicked = false
+
+let valMemStored = ''
+
+function numButPress(num) {}
+
+function mathButPress(operator) {}
+
+function equalButPress(num) {}
+
+function clearButPress() {}
+
+function copyButPress(num) {}
+
+function pasteButPress(num) {}
